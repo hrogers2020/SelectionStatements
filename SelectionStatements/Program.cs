8fe2ab1ead86891093b25c8accb90aa2f2b95db3
Hampton Rogers <rogershampton22@gmail.com>
2020-10-26 14:31:12 -0500

    Finished this quickly

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,27 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int favoriteNumber = 7;
+
+            Console.WriteLine("Guess my favorite number. Hint: below 12");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("Too LOW!!!");
+            }
+            else if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("Too HIGH!!!");
+            }
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("You are correct: Here is a cookie!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind"); 
+            }
         }
     }
 }
